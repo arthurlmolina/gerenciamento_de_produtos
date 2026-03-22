5f3477c6bf73a678c7009b48cd1cfc39dbbef170
Arthur L Molina <arthurlavagnollimolina@gmail.com>
2026-03-22 20:55:11 -0300

    model produto criado

--- src/main/java/com/example/gerenciamento_de_produtos/models/ProdutoModel.java
+++ src/main/java/com/example/gerenciamento_de_produtos/models/ProdutoModel.java
@@ -1,6 +1,54 @@
 package com.example.gerenciamento_de_produtos.models;
 
+import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="tb_produtos")
 public class ProdutoModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="name")
+    private String nome;
+    @Column(name="precoProduto")
+    private BigDecimal preco;
+    private Integer estoque;
+
+    public ProdutoModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
 }
